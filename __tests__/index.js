4df842988d8af022c1f0d1ce4f30a6f798b35d8c
Andrew Lisowski <andrew_lisowski@intuit.com>
2018-06-19 14:29:09 -0700

    test

--- __tests__/index.js
+++ __tests__/index.js
@@ -16,20 +16,3 @@ test('probeImageSize - no loaders previous', () => {
 
   expect(probeImageSize.bind({ resourcePath: '/' })(buffer)).toMatchSnapshot();
 });
-
-test('probeImageSize - some loaders previous', () => {
-  const buffer = Buffer.from(
-    `module.exports = {
-      src: __webpack_public_path__ + 'some/path/to/image.png',
-      lqip: 'path/to/lqip'
-    };`
-  );
-
-  jest.spyOn(fs, 'readFileSync').mockImplementationOnce(() => {});
-  jest.spyOn(probe, 'sync').mockImplementationOnce(() => ({
-    height: 100,
-    width: 200
-  }));
-
-  expect(probeImageSize.bind({ resourcePath: '/' })(buffer)).toMatchSnapshot();
-});
